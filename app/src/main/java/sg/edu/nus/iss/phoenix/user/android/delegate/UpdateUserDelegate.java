52711914b89f0fcab5c73742c38af715792f4de3
Rahul <rahultheagarajan@gmail.com>
2018-09-23 23:10:22 +0800

    Added Base Classes

---
+++ app/src/main/java/sg/edu/nus/iss/phoenix/user/android/delegate/UpdateUserDelegate.java
@@ -0,0 +1,8 @@
+package sg.edu.nus.iss.phoenix.user.android.delegate;
+
+/**
+ * Created by rahul on 23-09-2018.
+ */
+
+public class UpdateUserDelegate {
+}
---
+++ app/src/main/java/sg/edu/nus/iss/phoenix/user/entity/User.java
@@ -0,0 +1,8 @@
+package sg.edu.nus.iss.phoenix.user.entity;
+
+/**
+ * Created by rahul on 23-09-2018.
+ */
+
+public class User {
+}
