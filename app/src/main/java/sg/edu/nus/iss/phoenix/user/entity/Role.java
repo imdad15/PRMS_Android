d89c99d8885964bd5b9423730c9875725e5453b8
Rahul <rahultheagarajan@gmail.com>
2018-09-25 15:42:59 +0800

    Minor changes

--- app/src/main/java/sg/edu/nus/iss/phoenix/user/entity/Role.java
+++ app/src/main/java/sg/edu/nus/iss/phoenix/user/entity/Role.java
@@ -5,4 +5,23 @@ package sg.edu.nus.iss.phoenix.user.entity;
  */
 
 public class Role {
+
+    private String role;
+    private String accessPrivilege;
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getAccessPrivilege() {
+        return accessPrivilege;
+    }
+
+    public void setAccessPrivilege(String accessPrivilege) {
+        this.accessPrivilege = accessPrivilege;
+    }
 }
--- app/src/main/java/sg/edu/nus/iss/phoenix/user/entity/User.java
+++ app/src/main/java/sg/edu/nus/iss/phoenix/user/entity/User.java
@@ -3,6 +3,96 @@ package sg.edu.nus.iss.phoenix.user.entity;
 /**
  * Created by rahul on 23-09-2018.
  */
+import java.util.ArrayList;
 
 public class User {
+    private String id;
+    private String name;
+    private String password;
+    private String address;
+    private String dateOfJoining;
+    private String siteLink;
+    private ArrayList<Role> roles;
+    private boolean isEdit;
+
+    public User()
+    {
+
+    }
+
+    public User(String name, String password,String id, ArrayList<Role> role,String address, String doj,String siteLink) {
+        this.id = id;
+        this.name = name;
+        this.password = password;
+        this.roles = role;
+        this.address = address;
+        this.dateOfJoining = doj;
+        this.siteLink = siteLink;
+
+    }
+
+
+    public boolean isEdit() {
+        return isEdit;
+    }
+
+    public void setEdit(boolean edit) {
+        isEdit = edit;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public ArrayList<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(ArrayList<Role> roles) {
+        this.roles = roles;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getDateOfJoining() {
+        return dateOfJoining;
+    }
+
+    public void setDateOfJoining(String dateOfJoining) {
+        this.dateOfJoining = dateOfJoining;
+    }
+
+    public String getSiteLink() {
+        return siteLink;
+    }
+
+    public void setSiteLink(String siteLink) {
+        this.siteLink = siteLink;
+    }
 }
