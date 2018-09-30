52bba3555ec66b502adf0faf6bd450fdbec09c60
Rahul <rahultheagarajan@gmail.com>
2018-09-30 12:22:10 +0800

    .

--- app/src/main/java/sg/edu/nus/iss/phoenix/core/android/delegate/DelegateHelper.java
+++ app/src/main/java/sg/edu/nus/iss/phoenix/core/android/delegate/DelegateHelper.java
@@ -2,8 +2,8 @@ package sg.edu.nus.iss.phoenix.core.android.delegate;
 
 public class DelegateHelper {
     // RESTful parameters.
-    public final static String PRMS_BASE_URL_AUTHENTICATE = "http://192.168.1.67:8080/phoenix/rest/Login/doLogin?";
-    public final static String PRMS_BASE_URL_RADIO_PROGRAM = "http://192.168.1.67:8080/phoenix/rest/radioprogram";
-    public final static String PRMS_BASE_URL_SCHEDULE = "http://192.168.1.67:8080/phoenix/rest/scheduleprogram";
-    public final static String PRMS_BASE_URL_USERS = "http://192.168.1.67:8080/phoenix/rest/user";
+    public final static String PRMS_BASE_URL_AUTHENTICATE = "http://172.17.33.128:8080/phoenix/rest/Login/doLogin?";
+    public final static String PRMS_BASE_URL_RADIO_PROGRAM = "http://172.17.33.128:8080/phoenix/rest/radioprogram";
+    public final static String PRMS_BASE_URL_SCHEDULE = "http://172.17.33.128:8080/phoenix/rest/scheduleprogram";
+    public final static String PRMS_BASE_URL_USERS = "http://172.17.33.128:8080/phoenix/rest/user";
 }
